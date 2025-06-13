a2ccd7321383873a15c4255d5eb8a080ad6ef045
MakVieSAinte <makviesainte@gmail.com>
2025-06-14 00:58:36 +0100

    reejustement global

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,26 @@ module.exports = {
     "./**/*.js",     
     "./assets/css/**/*.css",  
   ],
-  theme: {
-    extend: {
-      fontSize: {
-        'base-mini':   '16px',
-        'base':   '24px',
-      },
-      colors: { 
-        'primary': '#2B892E', 
-        'secondary': '#3D3D3D', 
-        'secondary-two': '#BDBDBD', 
-        'secondary-three': '#DCDCDC', 
-      },
-      backgroundImage: {
-        'grid-bg': "url('./assets/images/grid-bg.png')",
-        'hero-bg': "url('./assets/images/bg-hero.png')",
-        'grid-bento': "url('./assets/images/grid-bento.png')",
-      },
-    },
-  },
+theme: {
+          extend: {
+            fontSize: {
+              "base-mini": "18px",
+              base: "27px",
+            },
+            colors: {
+              primary: "#2B892E",
+              secondary: "#3D3D3D",
+              "secondary-two": "#BDBDBD",
+              "secondary-three": "#DCDCDC",
+            },
+            backgroundImage: {
+              "grid-bg": "url('./assets/images/grid-bg.png')",
+              "hero-bg": "url('./assets/images/bg-hero.png')",
+              "grid-bento": "url('./assets/images/bento/grid-bento.png')",
+              "grid-bento-2": "url('./assets/images/bento/grid-bento-2.png')",
+            },
+          },
+        },
   plugins: [],
 }
 
